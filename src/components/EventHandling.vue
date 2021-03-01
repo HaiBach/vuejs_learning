667d7a2f37c518e54433ababf81923ca5818ea54
coder.haibach@gmail.com <coder.haibach@gmail.com>
2021-03-01 16:35:47 +0700

    Cập nhật file mới

---
+++ src/components/EventHandling.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="event-handling">
+    <p>{{ message }}</p>
+    <button v-on:click="reserveMessage">Reverse message</button>
+  </div>
+</template>
+
+
+<script setup>
+  const EventHandling = {
+    data() {
+    },
+    methods: {
+      reserveMessage() {
+        this.message = this.message
+          .split('')
+          .reverse()
+          .join('')
+      }
+    }
+  }
+
+  Vue.createApp(EventHandling).moute('#event-handling')
+</script>
